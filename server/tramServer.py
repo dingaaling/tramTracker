cec59430cc35e1929ba7840d5dbe2e962a254c89
Jon Cutler <jonleecutler@gmail.com>
2018-04-18 16:45:41 -0500

    Updates to server logic.

--- server/tramServer.py
+++ server/tramServer.py
@@ -6,10 +6,27 @@ import pyberrynet
 def main():
     """Tram Server"""
 
-    berrynet = pyberrynet.run()
-    results = berrynet.upload('picamera')
-    print(results)
-    berrynet.close()
+    berrynet = pyberrynet.run(warm_up=10, path='/home/pi/repos/BerryNet')
+    
+    try:
+        results = berrynet.upload('picamera')
+
+        if results == None:
+            return
+
+        results = results.replace('<br />', '\n')
+        results = results.split('\n')
+
+        for result in results:
+            if result == None or result == '':
+                continue
+            
+            print(result)
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        berrynet.close()
 
 if __name__ == '__main__':
     main()
