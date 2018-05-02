1b36bc4e149a51eb588f7503e3db77129bfc2ace
jonleecutler <31291496+jonleecutler@users.noreply.github.com>
2018-05-02 15:02:52 -0400

    Update interval based on testing.

--- server/tramState.py
+++ server/tramState.py
@@ -72,11 +72,11 @@ class TramState():
         
         # If it is a weekday check for rush hour
         if self._is_tram_rush_hour(current_time, current_weekday):
-            # 5 minutes of air time in seconds
-            return 300
+            # 6 minutes of air time in seconds
+            return 360
         
-        # 10 minutes of air time in seconds
-        return 600
+        # 11 minutes of air time in seconds
+        return 660
 
     def _is_tram_active(self, current_time, current_weekday):
         # 6:00am
